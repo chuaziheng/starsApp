637b31783e911c3060569ca892a751b251d0cac6
NicholasGoh <gohn0004@e.ntu.edu.sg>
2020-11-25 13:51:26 +0800

    javadoc

--- AdminStudentManagement.java
+++ AdminStudentManagement.java
@@ -1,3 +1,9 @@
+/**
+ * Interface for Admin that manages Student related methods
+ * @author  Zi Heng
+ * @version 1.0
+ * @since   2020-11-20
+ * */
 public interface AdminStudentManagement{
 	public abstract void addStudent();
 	public abstract void deleteStudent() throws Exception;
